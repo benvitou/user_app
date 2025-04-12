9e339bb218a2826b91c03544078fcf6b39cc79aa
benvitou <benvitou00168@gmail.com>
2025-04-12 15:25:28 +0700

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,15 +1,15 @@
 function Navbar({ onAddClick }) {
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-info shadow-sm">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark shadow-sm">
       <div className="container">
         <a className="navbar-brand fw-bold" href="/">
           <i class="fa-solid fa-music"></i>
-          Music App
+          User App
         </a>
 
         <button className="btn btn-light ms-auto" onClick={onAddClick}>
           <i className="bi bi-plus-circle me-2"></i>
-          Add new Music
+          Add new User
         </button>
       </div>
     </nav>
